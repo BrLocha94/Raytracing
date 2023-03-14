2c97794dd99addfb456c364aa8cdb911f519718f
BrunoLocha <bruno.locha@hydragames.com.br>
2023-03-14 18:28:45 -0300

    Created ray class and basic viewport camera

--- Raytracing/Vector3.h
+++ Raytracing/Vector3.h
@@ -22,29 +22,34 @@ class Vector3
 		double operator[](int i) const { return e[i]; }
 		double& operator[](int i) { return e[i]; }
 
-		Vector3& operator+=(const Vector3& v) {
+		Vector3& operator+=(const Vector3& v) 
+		{
 			e[0] += v.e[0];
 			e[1] += v.e[1];
 			e[2] += v.e[2];
 			return *this;
 		}
 
-		Vector3& operator*=(const double t) {
+		Vector3& operator*=(const double t) 
+		{
 			e[0] *= t;
 			e[1] *= t;
 			e[2] *= t;
 			return *this;
 		}
 
-		Vector3& operator/=(const double t) {
+		Vector3& operator/=(const double t) 
+		{
 			return *this *= 1 / t;
 		}
 
-		double length() const {
+		double length() const 
+		{
 			return sqrt(length_squared());
 		}
 
-		double length_squared() const {
+		double length_squared() const 
+		{
 			return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
 		}
 
@@ -59,46 +64,56 @@ using color = Vector3; // RGB COLOR
 
 
 //Vector 3 utility functions
-inline std::ostream& operator<<(std::ostream& out, const Vector3& v) {
+inline std::ostream& operator<<(std::ostream& out, const Vector3& v) 
+{
 	return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
-inline Vector3 operator+(const Vector3& u, const Vector3& v) {
+inline Vector3 operator+(const Vector3& u, const Vector3& v) 
+{
 	return Vector3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
-inline Vector3 operator-(const Vector3& u, const Vector3& v) {
+inline Vector3 operator-(const Vector3& u, const Vector3& v)
+{
 	return Vector3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline Vector3 operator*(const Vector3& u, const Vector3& v) {
+inline Vector3 operator*(const Vector3& u, const Vector3& v) 
+{
 	return Vector3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline Vector3 operator*(double t, const Vector3& v) {
+inline Vector3 operator*(double t, const Vector3& v) 
+{
 	return Vector3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
-inline Vector3 operator*(const Vector3& v, double t) {
+inline Vector3 operator*(const Vector3& v, double t) 
+{
 	return t * v;
 }
 
-inline Vector3 operator/(Vector3 v, double t) {
+inline Vector3 operator/(Vector3 v, double t)
+{
 	return (1 / t) * v;
 }
 
-inline double dot(const Vector3& u, const Vector3& v) {
+inline double dot(const Vector3& u, const Vector3& v) 
+{
 	return u.e[0] * v.e[0]
 		+ u.e[1] * v.e[1]
 		+ u.e[2] * v.e[2];
 }
 
-inline Vector3 cross(const Vector3& u, const Vector3& v) {
+inline Vector3 cross(const Vector3& u, const Vector3& v) 
+{
 	return Vector3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
 		u.e[2] * v.e[0] - u.e[0] * v.e[2],
 		u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline Vector3 unit_vector(Vector3 v) {
+inline Vector3 unit_vector(Vector3 v) 
+{
 	return v / v.length();
 }
